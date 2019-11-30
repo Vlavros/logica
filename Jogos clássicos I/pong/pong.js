92bf803e94d9a9ea4421f931b60d7bd61d48c952
Vlavros <vlavros@home.com>
2019-11-30 00:18:46 -0300

    Melhorias
    melhorias no pong

--- Jogos clássicos I/pong/pong.js
+++ Jogos clássicos I/pong/pong.js
@@ -21,13 +21,21 @@ let oXRaquete = 575;
 let oYRaquete = 140;
 let oHRaquete = 80; //tamanho da raquete (height)
 let oWRaquete = 15; //largura da raquete (width)
+let oVelocidadeY;
 
 let meusPontos = 0;
 let oponetePontos = 0;
 
+function preload() {
+  soundFormats('mp3', 'ogg');
+  trilhaSound = loadSound('assets/trilha.mp3');
+  raquetadaSound = loadSound('assets/raquetada.mp3');
+  pontoSound = loadSound('assets/ponto.mp3');
+}
 
 function setup() {
   createCanvas(xCanva, yCanva);
+  tocarBackgroundSound();
 }
 
 function draw() {
@@ -42,15 +50,27 @@ function draw() {
   moverMinhaRaquete();
   moverOponenteRaquete();
   
-  textSize(32);
-  text(meusPontos + ' x ' + oponetePontos, 250, 30);
-  fill(255);
+  colidirRaquete();
+  
+  montarPlacar();   
+  
+  
+}
+
+function tocarBackgroundSound() {
+
+  trilhaSound.setVolume(0.1);
+  trilhaSound.play();
   
 }
 
 function moverBola() {
   if(xBola > width-rBola || xBola-rBola < 0) {
     velocidadeXBola *= -1;
+
+    pontoSound.setVolume(0.1);
+    pontoSound.play();
+    
     if(xBola > width-rBola) {
       meusPontos += 1;
     } else {
@@ -61,17 +81,34 @@ function moverBola() {
   if(yBola > height-rBola || yBola-rBola < 0) {
     velocidadeYBola *= -1;
   }
+  
+  xBola += velocidadeXBola;
+  yBola += velocidadeYBola;
+}
 
+function montarPlacar() {
+  
+  fill(color(255,140,0));
+  rect(240,4,90,30);
+  
+  textSize(32);
+  fill(255);
+  text(meusPontos + ' x ' + oponetePontos, 250, 30);
+  
+}
+
+function colidirRaquete() {
   if((xBola-rBola <= mXRaquete+mWRaquete && 
       (yBola <= mYRaquete+mHRaquete && yBola >= mYRaquete)) || 
       (xBola >= oXRaquete && 
       (yBola-rBola <= oYRaquete+mHRaquete && yBola >= oYRaquete))){
       
     velocidadeXBola *= -1;  
+    
+    raquetadaSound.setVolume(0.1);
+    raquetadaSound.play();
+    
   } 
-  
-  xBola += velocidadeXBola;
-  yBola += velocidadeYBola;
 }
 
 function moverMinhaRaquete() {
@@ -86,11 +123,21 @@ function moverMinhaRaquete() {
 
 function moverOponenteRaquete() {
   
+  let r = 30//random(30,60);
+  
+  oVelocidadeY = yBola - oYRaquete - oHRaquete / 2 - r;
+  
+  if((oYRaquete + oVelocidadeY) > 0 && (oYRaquete + oVelocidadeY) < height) {
+    oYRaquete += oVelocidadeY;
+  }
+  
+  /*
   if(yBola > oYRaquete && oYRaquete+oHRaquete < height){
     oYRaquete += velocidade;
   } else if(yBola < oYRaquete && oYRaquete > 0) {
     oYRaquete -= velocidade;
   }
+  */
 }
 
 
